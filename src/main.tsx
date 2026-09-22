ff87b8a6cd9164f059c3e884f06caf0facdf8f71
QuantumBen <omosabenedict@gmail.com>
2026-09-22 12:43:12 +0100

    Connecting Redux to React

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router'
+
+import App from './App'
+import { store } from './app/store'
+
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
+
+
+//<StrictMode> Helps React detect certain development problems.
+// <Provider> This comes from:react-redux -- Make our Redux store available to everything inside this application.
+// <BrowserRouter> makes routing possible.
