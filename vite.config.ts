b7344ee93a0af1d392ef6501044bdb517803da54
QuantumBen <omosabenedict@gmail.com>
2026-09-22 12:25:35 +0100

    configured tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite' //We use it to configure Vite.
+import react from '@vitejs/plugin-react' //This brings React support into Vite.
+import tailwindcss from '@tailwindcss/vite' //This imports Tailwind's Vite plugin.
 
 export default defineConfig({
   plugins: [
